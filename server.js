8e271056d7ee48b3bdb14e6e633229894f0d6ace
Rahulsharma468 <sharmarahul1729@gmail.com>
2022-03-31 11:27:58 +0530

    Auth

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use('/' , require('./routes/auth.routes'));
+app.use('/routes' , require('./routes/auth.routes'));
 
 app.listen(5000 , () => {
     console.log('App is running on 5000');
